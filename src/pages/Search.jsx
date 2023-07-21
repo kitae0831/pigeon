d174bfbc9b5f9d56609b2704363bd4c69763b593
Park kitae <bkt4044@naver.com>
2023-07-21 16:10:47 +0900

    animalTest 페이지 호환이 안되서  Router 수정

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -7,17 +7,17 @@ import { useLocation, useParams } from 'react-router-dom';
 function Search() {
   const { id } = useParams();
 
-  let componentToRender;
+  let seperateType;
   if (id === 'dog') {
-    componentToRender = <Dog />;
+    seperateType = <Dog />;
   } else if (id === 'cat') {
-    componentToRender = <Cat />;
+    seperateType = <Cat />;
   } else {
     // id가 지정되지 않은 경우 또는 잘못된 값인 경우 기본적으로 Bird 컴포넌트를 렌더링합니다.
-    componentToRender = <Bird />;
+    seperateType = <Bird />;
   }
 
-  return <div>{componentToRender}</div>;
+  return <div>{seperateType}</div>;
 }
 
 export default Search;
